5a9b71e0730374698d130430330b72761dd3883a
volobolo99 <volobolo99@gmail.com>
2026-08-25 18:13:19 +0200

    chore(static-data): finalize package exports

--- app/static_data/__init__.py
+++ app/static_data/__init__.py
@@ -1,5 +1,15 @@
-"""Static game-data ingestion and runtime snapshot interfaces."""
+"""NosAi static and online data intelligence layer."""
 
+from .data_gateway import CachePolicy, DataGateway
 from .manifest import StaticDataset, StaticManifest
+from .providers import HTTPDataProvider, ProviderConfig, ProviderError
 
-__all__ = ["StaticDataset", "StaticManifest"]
+__all__ = [
+    "CachePolicy",
+    "DataGateway",
+    "HTTPDataProvider",
+    "ProviderConfig",
+    "ProviderError",
+    "StaticDataset",
+    "StaticManifest",
+]
