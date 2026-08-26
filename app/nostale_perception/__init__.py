bcc2d7e43e7a54f468ed45b4d7955d48b10c9796
volobolo99 <volobolo99@gmail.com>
2026-08-26 14:25:22 +0200

    feat(perception): expose safe window discovery

--- app/nostale_perception/__init__.py
+++ app/nostale_perception/__init__.py
@@ -3,12 +3,13 @@
 from .gamestate import GameState, GameStateBuilder, PlayerState, WorldEntity
 from .perception import Frame, FrameSource, ObservationPipeline, PerceptionResult
 from .replay import ReplayFrame, ReplayFrameSource, write_jsonl
+from .window_discovery import WindowCandidate, discover_windows
 from .windows_backend import Win32WindowCaptureBackend
 from .windows_capture import WindowsFrameSource, WindowsWindowTarget
 
 __all__ = [
     "Frame", "FrameSource", "GameState", "GameStateBuilder", "PlayerState",
     "WorldEntity", "ObservationPipeline", "PerceptionResult", "ReplayFrame",
-    "ReplayFrameSource", "write_jsonl", "WindowsFrameSource", "WindowsWindowTarget",
-    "Win32WindowCaptureBackend",
+    "ReplayFrameSource", "write_jsonl", "WindowCandidate", "discover_windows",
+    "WindowsFrameSource", "WindowsWindowTarget", "Win32WindowCaptureBackend",
 ]
