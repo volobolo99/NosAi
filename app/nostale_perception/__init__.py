ed56f24206cdebf9049e7cbda2804cf8f76cf0e8
volobolo99 <volobolo99@gmail.com>
2026-08-26 14:28:15 +0200

    feat(perception): expose calibration and UI detector

--- app/nostale_perception/__init__.py
+++ app/nostale_perception/__init__.py
@@ -1,8 +1,10 @@
 """Read-only NosTale perception and GameState normalization layer."""
 
+from .calibration import BarCalibration, CalibrationProfile, PlayerUiCalibration, Roi
 from .gamestate import GameState, GameStateBuilder, PlayerState, WorldEntity
 from .perception import Frame, FrameSource, ObservationPipeline, PerceptionResult
 from .replay import ReplayFrame, ReplayFrameSource, write_jsonl
+from .ui_detector import BarObservation, PixelBarDetector
 from .window_discovery import WindowCandidate, discover_windows
 from .windows_backend import Win32WindowCaptureBackend
 from .windows_capture import WindowsFrameSource, WindowsWindowTarget
@@ -10,6 +12,8 @@ from .windows_capture import WindowsFrameSource, WindowsWindowTarget
 __all__ = [
     "Frame", "FrameSource", "GameState", "GameStateBuilder", "PlayerState",
     "WorldEntity", "ObservationPipeline", "PerceptionResult", "ReplayFrame",
-    "ReplayFrameSource", "write_jsonl", "WindowCandidate", "discover_windows",
-    "WindowsFrameSource", "WindowsWindowTarget", "Win32WindowCaptureBackend",
+    "ReplayFrameSource", "write_jsonl", "Roi", "BarCalibration", "PlayerUiCalibration",
+    "CalibrationProfile", "BarObservation", "PixelBarDetector", "WindowCandidate",
+    "discover_windows", "WindowsFrameSource", "WindowsWindowTarget",
+    "Win32WindowCaptureBackend",
 ]
