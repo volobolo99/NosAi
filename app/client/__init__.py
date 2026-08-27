f401c019eb9fac2c839874fbfe86748b9183f16f
volobolo99 <volobolo99@gmail.com>
2026-08-27 20:19:49 +0200

    feat(g3.25): export layered runtime adapter contract

--- app/client/__init__.py
+++ app/client/__init__.py
@@ -2,10 +2,14 @@
 
 from .adapter import ClientAdapter, ClientState
 from .nostale_windows import NosTaleClientError, WindowsNosTaleAdapter
+from .runtime_adapter import AdapterMode, RuntimeAdapter, RuntimeObservation
 
 __all__ = [
+    "AdapterMode",
     "ClientAdapter",
     "ClientState",
     "NosTaleClientError",
+    "RuntimeAdapter",
+    "RuntimeObservation",
     "WindowsNosTaleAdapter",
 ]
