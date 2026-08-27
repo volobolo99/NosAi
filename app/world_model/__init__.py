a5e327053ec4650381359a2cf4959a4557bb050b
volobolo99 <volobolo99@gmail.com>
2026-08-27 06:18:31 +0200

    feat(block-a): expose minimap world mapper

--- app/world_model/__init__.py
+++ app/world_model/__init__.py
@@ -3,18 +3,13 @@
 from .actions import WorldAction
 from .entity_tracker import EntityTracker, TrackingConfig
 from .hud_state import HudStateExtractor, HudValue
+from .minimap_world import MinimapCalibration, MinimapWorldMapper
 from .model import WorldModel
 from .observation_mapper import ObservationMapper
 from .state import EntityState, WorldState
 
 __all__ = [
-    "EntityState",
-    "EntityTracker",
-    "HudStateExtractor",
-    "HudValue",
-    "ObservationMapper",
-    "TrackingConfig",
-    "WorldAction",
-    "WorldModel",
-    "WorldState",
+    "EntityState", "EntityTracker", "HudStateExtractor", "HudValue",
+    "MinimapCalibration", "MinimapWorldMapper", "ObservationMapper",
+    "TrackingConfig", "WorldAction", "WorldModel", "WorldState",
 ]
