bd2e820e3deb02b3aab184c95a6d0899ae145364
volobolo99 <volobolo99@gmail.com>
2026-08-28 13:15:35 +0200

    feat(party): add NosMate Partner domain package

---
+++ app/party/__init__.py
@@ -0,0 +1,25 @@
+"""Unified NosMate Pet + cognitive Partner party-domain models."""
+
+from .nosmate_partner import (
+    BehaviorState,
+    CommandRisk,
+    PartnerDecisionEvaluator,
+    PartnerEntity,
+    PartnerMemory,
+    PartyCommand,
+    RelationshipTier,
+    SkillRank,
+    SpecialistPartnerCard,
+)
+
+__all__ = [
+    "BehaviorState",
+    "CommandRisk",
+    "PartnerDecisionEvaluator",
+    "PartnerEntity",
+    "PartnerMemory",
+    "PartyCommand",
+    "RelationshipTier",
+    "SkillRank",
+    "SpecialistPartnerCard",
+]
