c612bbabc11ab01bcc042ad17a65bfa31e9a9fd9
volobolo99 <volobolo99@gmail.com>
2026-08-28 08:34:27 +0200

    feat(guardai): configure Android build toolchain

---
+++ android_client/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.protobuf") version "0.9.4" apply false
+}
