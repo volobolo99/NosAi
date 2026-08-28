c0812b85345b4af8da0fed6f6ffc69321512e093
volobolo99 <volobolo99@gmail.com>
2026-08-28 09:16:00 +0200

    feat(guardai): expose telemetry synchronizer API

---
+++ app/guardai/__init__.py
@@ -0,0 +1,5 @@
+"""GuardAi runtime integration components."""
+
+from .telemetry_synchronizer import FrameTelemetryPair, GuardAiTelemetrySynchronizer
+
+__all__ = ["FrameTelemetryPair", "GuardAiTelemetrySynchronizer"]
